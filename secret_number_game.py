80719d34368c09626156568f2e0454ad573f48da
llekcevi <69637064+llekcevi@users.noreply.github.com>
2022-04-19 22:35:54 +0200

    Update secret_number_game.py

--- secret_number_game.py
+++ secret_number_game.py
@@ -1,12 +1,12 @@
-print("SECRET NUMBER \n")
-
-player_name = input("enter your name: ")        # dodati: 1) bodovanje i praćenje bodova
-difficulty = input("Choose difficulty:\n"       # 2) bonus za 20 bodova - pomoć je li broj (ne)paran
-                   " easy (1 - 10), \n"         # 3) za pogodak isprve dati duple bodove
+print("SECRET NUMBER \n")                       # dodati: 1) bodovanje i praćenje bodova  2) bonus za 20 bodova - pomoć je li broj (ne)paran
+                                                # 3) za pogodak isprve dati duple bodove
+player_name = input("enter your name: ")        
+difficulty = input("Choose difficulty:\n"       #choose difficulty
+                   " easy (1 - 10), \n"        
                    "normal (1 - 20) \n"
                    "or hard? (1 - 50)")
 if difficulty == "easy":
-    numbers_range = list(range(1, 11))
+    numbers_range = list(range(1, 11))      # each difficulty has a max number that you can guess
     max_choice = 10
     win_points = 1
 elif difficulty == "normal":
@@ -35,8 +35,8 @@ def play_secret_number():
                   f"the secret number was {secret_number} \n"
                   f"you took {number_of_guesses} tries \n"
                   f"you won {win_points} points")
-        elif user_guess > max_choice or user_guess < 1:
-            print(f"{user_guess} is not a number between 1 and {max_choice}. try again")
+        elif user_guess > max_choice or user_guess < 1:                                       # checks that user has entered a number from allowed range
+            print(f"{user_guess} is not a number between 1 and {max_choice}. try again")      # this is where max number helps us
         else:
             if user_guess < secret_number:
                 print("too low, try again")
@@ -49,7 +49,7 @@ again = True
 play_secret_number()
 
 while again:
-    repeat = input(f"{player_name}, do you want to play again? (Type yes or no.) \n")
+    repeat = input(f"{player_name}, do you want to play again? (Type yes or no.) \n")           # dodati kontrolu unosa za ostale stvari
     if repeat == "no":
         again = False
         print("\n Game ended. \n")
