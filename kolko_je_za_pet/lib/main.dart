b54f5d26d23622d331a17b5847c5a4c2436e1716
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-11-29 14:12:47 +0100

    Added firebase to app

--- kolko_je_za_pet/lib/main.dart
+++ kolko_je_za_pet/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/input_points.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: KolkoJeZaPet()));
 }
 
