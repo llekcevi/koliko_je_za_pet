11d38aac0816e30d16ab83480617c528b8963af7
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-07 19:46:44 +0100

    Added filterRazred and updated readExams to FutureBuilder

--- kolko_je_za_pet/lib/database_functions.dart
+++ kolko_je_za_pet/lib/database_functions.dart
@@ -11,5 +11,11 @@ Future writeExam(Exam exam) async {
   ispiti.doc(documentName).set(exam.toJson());
 }
 
-Stream<List<Exam>> readExams() => ispiti.snapshots().map((snapshot) =>
-    snapshot.docs.map((doc) => Exam.fromJson(doc.data())).toList());
+Future<List<Exam>> readExams() =>
+    ispiti.orderBy("razred", descending: false).get().then((snapshot) =>
+        snapshot.docs.map((doc) => Exam.fromJson(doc.data())).toList());
+
+Future<List<Exam>> filterRazred(int razred) => ispiti
+    .where("razred", isEqualTo: razred)
+    .get()
+    .then((value) => value.docs.map((e) => Exam.fromJson(e.data())).toList());
