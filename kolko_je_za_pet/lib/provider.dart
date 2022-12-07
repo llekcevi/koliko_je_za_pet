f29b1f073506440b9af16ce89c0ef7cc7b31ea23
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-07 18:20:51 +0100

    Added providers for TextEditingControllers

---
+++ kolko_je_za_pet/lib/provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nazivProvider = Provider((ref) => TextEditingController());
+
+final razredProvider = Provider((ref) => TextEditingController());
+
+final bodoviProvider = Provider((ref) => TextEditingController());
