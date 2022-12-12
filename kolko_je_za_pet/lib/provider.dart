df2fb005fff0913e02e0c20a5db7ba32db049ba2
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-13 00:59:14 +0100

    Added comments and provider for filter menu

--- kolko_je_za_pet/lib/provider.dart
+++ kolko_je_za_pet/lib/provider.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//Providers for input fields
+//--------------------------
 final nazivProvider = Provider((ref) => TextEditingController());
 
 final razredProvider = Provider((ref) => TextEditingController());
 
 final bodoviProvider = Provider((ref) => TextEditingController());
 
-void clearProviders(
+void clearTextEditingProviders(
     TextEditingController nazivController,
     TextEditingController razredController,
     TextEditingController bodoviController) {
@@ -15,3 +17,16 @@ void clearProviders(
   razredController.clear();
   bodoviController.clear();
 }
+//--------------------------
+
+//Providers for filter menu
+//-------------------------
+class RazredFilterState extends StateNotifier<int> {
+  RazredFilterState() : super(0);
+  void chooseRazredFilter(int razred) => state = razred;
+  int get getRazredFilter => state;
+}
+
+final razredFilterProvider =
+    StateNotifierProvider<RazredFilterState, int>((ref) => RazredFilterState());
+//-------------------------
