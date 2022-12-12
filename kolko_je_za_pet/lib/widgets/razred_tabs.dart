470ecce7591c6b6a7c42e6b811e44ef2a396db83
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-13 00:58:47 +0100

    Converted to ConsumerWidget and added some styling

--- kolko_je_za_pet/lib/widgets/razred_tabs.dart
+++ kolko_je_za_pet/lib/widgets/razred_tabs.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kolko_je_za_pet/database_functions.dart';
 import 'package:kolko_je_za_pet/model.dart';
-import 'package:kolko_je_za_pet/widgets/grades_column.dart';
+import 'package:kolko_je_za_pet/provider.dart';
 
-class RazredTabs extends StatelessWidget {
+class RazredTabs extends ConsumerWidget {
   const RazredTabs({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var future = readExams();
+    final selectedRazred = ref.watch(razredFilterProvider);
+    final razredProvider = ref.read(razredFilterProvider.notifier);
     return FutureBuilder<List<Exam>>(
       future: future,
       builder: (context, snapshot) {
@@ -25,17 +28,26 @@ class RazredTabs extends StatelessWidget {
                 : null;
           }
           return ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: razredi.length,
-              itemBuilder: (context, index) => Container(
-                    color: Colors.grey,
-                    width: MediaQuery.of(context).size.width / 4,
-                    child: ListTile(
-                      title: Text(
-                          textAlign: TextAlign.center,
-                          razredi[index].toString()),
-                    ),
-                  ));
+            scrollDirection: Axis.horizontal,
+            itemCount: razredi.length,
+            itemBuilder: (context, index) => Container(
+              width: MediaQuery.of(context).size.width / 4,
+              child: ListTile(
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30))),
+                selected: selectedRazred == razredi[index] ? true : false,
+                tileColor: Theme.of(context).highlightColor,
+                selectedTileColor: Colors.white,
+                onTap: () {
+                  razredProvider.chooseRazredFilter(razredi[index]);
+                },
+                title: Text(
+                    textAlign: TextAlign.center, razredi[index].toString()),
+              ),
+            ),
+          );
         } else {
           return const Center(
             child: CircularProgressIndicator(),
@@ -45,9 +57,3 @@ class RazredTabs extends StatelessWidget {
     );
   }
 }
-
-Widget buildExam(Exam exam) => ExpansionTile(
-      title: Text("${exam.naziv}"),
-      subtitle: Text(exam.razred.toString()),
-      children: [GradesColumn(exam: exam)],
-    );
