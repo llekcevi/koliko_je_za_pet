b56a1026f1ca6c80fd7c76e50fa026c2726b2b4f
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-13 00:58:10 +0100

    Converted to ConsumerWidget

--- kolko_je_za_pet/lib/widgets/filter_exams.dart
+++ kolko_je_za_pet/lib/widgets/filter_exams.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kolko_je_za_pet/database_functions.dart';
 import 'package:kolko_je_za_pet/screens/exams_list.dart';
+import 'package:kolko_je_za_pet/provider.dart';
 
-class FilterExams extends StatefulWidget {
+class FilterExams extends ConsumerWidget {
   const FilterExams({super.key});
 
   @override
-  State<FilterExams> createState() => _FilterExamsState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final razred = ref.watch(razredFilterProvider);
 
-class _FilterExamsState extends State<FilterExams> {
-  @override
-  Widget build(BuildContext context) {
     return FutureBuilder(
-        future: filterRazred(6),
+        future: filterRazred(razred),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return const Text("something went wrong");
