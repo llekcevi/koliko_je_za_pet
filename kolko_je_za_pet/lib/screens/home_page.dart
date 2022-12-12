40d4f6869d9f07a01831616aa2893e9f03552f13
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-13 00:56:30 +0100

    Removed unnecessary imports

--- kolko_je_za_pet/lib/screens/home_page.dart
+++ kolko_je_za_pet/lib/screens/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kolko_je_za_pet/database_functions.dart';
 import 'package:kolko_je_za_pet/widgets/filter_exams.dart';
 import 'package:kolko_je_za_pet/widgets/razred_tabs.dart';
-import 'exams_list.dart';
 import '../widgets/input_points_alert_dialog.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,23 +9,31 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
           child: Center(
-              child: Column(
-        children: [
-          Container(width: double.infinity, height: 50, child: RazredTabs()),
-          Expanded(child: ExamsList(future: readExams())),
-          FloatingActionButton.extended(
-              icon: const Icon(Icons.add),
-              label: const Text("Dodaj novi ispit"),
-              onPressed: () => showDialog(
-                  context: context,
-                  builder: (BuildContext context) => const InputPoints()))
-        ],
-      ))),
-    ));
-    ;
+            child: Column(
+              children: [
+                const SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: RazredTabs(),
+                ),
+                const Expanded(
+                  child: FilterExams(),
+                ),
+                FloatingActionButton.extended(
+                    icon: const Icon(Icons.add),
+                    label: const Text("Dodaj novi ispit"),
+                    onPressed: () => showDialog(
+                        context: context,
+                        builder: (BuildContext context) => const InputPoints()))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
