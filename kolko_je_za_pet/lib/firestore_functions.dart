342e3d97f1d2d0619fb36cca5f875b5b9e334788
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-01-05 16:58:13 +0100

    Added function to sync data between device and firestore

--- kolko_je_za_pet/lib/firestore_functions.dart
+++ kolko_je_za_pet/lib/firestore_functions.dart
@@ -1,10 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:kolko_je_za_pet/hive_functions.dart';
 import 'model.dart';
-import 'provider.dart';
+import 'package:hive/hive.dart';
 
 final db = FirebaseFirestore.instance;
 final ispiti = db.collection("ispiti");
 final users = db.collection("users");
+
+Future syncExams(String userId, Box hiveBox) async {
+  final local = hiveBox.values;
+
+  for (Exam exam in local) {
+    final documentName =
+        "${exam.naziv.toString().replaceAll(" ", "")}${exam.razred}";
+
+    users.doc(userId).collection("ispiti").doc(documentName).set(exam.toJson());
+  }
+
+  final cloud = users
+      .doc(userId)
+      .collection("ispiti")
+      .get()
+      .then((value) => value.docs.map((e) => Exam.fromJson(e.data())).toList());
+
+  cloud.then((listOfFirestoreExams) {
+    for (Exam firestoreExam in listOfFirestoreExams) {
+      final hiveList = hiveBox.values.toList();
+      final duplicateExams = hiveList
+          .where((hiveExam) => hiveExam.naziv == firestoreExam.naziv)
+          .toList();
+      if (duplicateExams.isEmpty) {
+        hiveBox.add(firestoreExam);
+        print("added ${firestoreExam.toString()}");
+      } else {
+        print("not added");
+      }
+    }
+
+    //print(hiveBox.values.toList());
+  });
+}
 
 Future writeExamUser(Exam exam, String userID) async {
   final usersExams = users.doc(userID).collection("ispiti");
