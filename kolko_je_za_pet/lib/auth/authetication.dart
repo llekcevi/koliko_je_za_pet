d8ce6678efe185a1e427e7f50b6e51fc355ba29e
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-01-05 16:55:08 +0100

    added buttons to sync files

--- kolko_je_za_pet/lib/auth/authetication.dart
+++ kolko_je_za_pet/lib/auth/authetication.dart
@@ -1,26 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hive/hive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kolko_je_za_pet/provider.dart';
+import 'package:kolko_je_za_pet/firestore_functions.dart';
 
 class Authetication extends ConsumerWidget {
   const Authetication({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final examsBox = Hive.box("exams");
     final google = ref.read(googleSignInProvider);
-    //GoogleSignInAccount? user = google.user;
-
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //Text(user.displayName!),
-          ElevatedButton(
-            child: Text("google login"),
-            onPressed: () => google.googleLogin(),
-          ),
-        ],
+    final userStateChange = FirebaseAuth.instance.authStateChanges();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            StreamBuilder(
+                stream: userStateChange,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(google.currentUser!.displayName!),
+                        ElevatedButton(
+                            onPressed: (() {
+                              final id = google.currentUser!.uid;
+                              syncExams(id, examsBox);
+                            }),
+                            child: Text("sinkroniziraj")),
+                        ElevatedButton(
+                            child: Text("google logout"),
+                            onPressed: () {
+                              FirebaseAuth.instance.signOut();
+                            }),
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  } else {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            child: Text("google login"),
+                            onPressed: () {
+                              google.googleLogin();
+                            }),
+                      ],
+                    );
+                  }
+                }),
+          ],
+        ),
       ),
     );
   }
