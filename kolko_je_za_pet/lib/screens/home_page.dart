4fd92ad23c8173f59cc70edcf60a5be504c58a17
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-01-05 16:55:40 +0100

    Removed unnecessary imports and widgets

--- kolko_je_za_pet/lib/screens/home_page.dart
+++ kolko_je_za_pet/lib/screens/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:kolko_je_za_pet/hive_functions.dart';
-import 'package:kolko_je_za_pet/widgets/exams_by_razred.dart';
-import 'package:kolko_je_za_pet/widgets/hive/all_hive_exams.dart';
-import 'package:kolko_je_za_pet/widgets/razred_tabs.dart';
+import 'package:kolko_je_za_pet/widgets/hive/hive_exams_by_razred.dart';
 import 'package:kolko_je_za_pet/widgets/buttons/add_new_exam_floating_action_button.dart';
 import 'package:kolko_je_za_pet/auth/authetication.dart';
+
+import '../widgets/hive/hive_razred_filters.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -34,10 +34,9 @@ class HomePage extends StatelessWidget {
           child: Center(
             child: Column(
               children: const [
-                //RazredTabs(),
                 //ExamsByRazred(),
                 HiveRazredFilters(),
-                Expanded(child: HiveExamsByRazred()),
+                HiveExamsByRazred(),
                 AddNewExamFloatingActionButton()
               ],
             ),
