6c13389d19c11da096b00567ec01178345b09923
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-02-19 12:59:24 +0100

    Removed unnecessary imports and initialization of firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:kolko_je_za_pet/model.dart';
-import 'firebase_options.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'screens/home_page.dart';
@@ -14,10 +12,6 @@ void main() async {
       await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDirectory.path);
   Hive.registerAdapter(ExamAdapter());
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   runApp(ProviderScope(
     child: MaterialApp(
