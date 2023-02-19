1607cee78ea8e803c02c67fc7720ecd55b15988a
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-02-19 17:15:56 +0100

    removed firebaase packages and dependencies

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 //Providers for input fields
 //--------------------------
